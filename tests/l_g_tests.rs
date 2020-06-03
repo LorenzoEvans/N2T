a61355663bcfe88616450ffbc7dcff5022a5d8c2
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-06-02 22:00:24 -0700

    Lots of ones and 0's afoot rn

--- tests/l_g_tests.rs
+++ tests/l_g_tests.rs
@@ -1,12 +1,25 @@
 #[cfg(test)]
 mod l_g_tests {
-    use nand2_tetris::{nand, or_nand, and, or};
+    use nand2_tetris::{
+        nand, // tested
+        or_nand, // tested
+        and, // tested
+        and_16,
+        or, // tested
+        or_16,
+        or_8w,
+        mux,
+        mux_16,
+        mux_16_4w,
+        mux_16_8w,
+        dmux_4w,
+        dmux_8w,
+        xor,
+        not, // tested
+        not_16
+    };
     // let out = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-    // let and_16_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-    // let and_16_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
-    // let or_8w_arr_1 = [0, 0, 0, 0];
-    // let or_8w_arr_2 = [ 1, 1, 0, 0];
-    // let mut or_out_1 = [1, 1, 1, 1];
+    
     #[test]
     fn test_nand() {
         let nand_1 = (1, 1);
@@ -42,6 +55,11 @@ mod l_g_tests {
         assert_eq!(and_gate_3, 0);
     }
     #[test]
+    fn test_and_16() {
+        let and_16_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let and_16_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+    }
+    #[test]
     fn test_or() {
         let or_gate_1 = or(0, 0);
         let or_gate_2 = or(1, 0);
@@ -53,4 +71,69 @@ mod l_g_tests {
         assert_eq!(or_gate_3, 1);
         assert_eq!(or_gate_4, 1);
     }
+    #[test]
+    fn test_xor() {
+        \
+    }
+    #[test]
+    fn test_or_16() {
+
+    }
+    #[test]
+    fn test_or_8w() {
+        let or_8w_arr_1 = [0, 0, 0, 0];
+        let or_8w_arr_2 = [ 1, 1, 0, 0];
+    }
+    #[test]
+    fn test_mux() {
+
+    }
+    #[test]
+    fn test_mux_16() {
+        let mux_16_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let out = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+    }
+    #[test]
+    fn test_mux_16_4w() {
+        let mux_16_4w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_4w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let mux_16_4w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_4w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let out = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+    }
+    #[test]
+    fn test_mux_16_8w() {
+        let mux_16_8w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_8w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let mux_16_8w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_8w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let mux_16_8w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_8w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let mux_16_8w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_8w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let mux_16_8w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+        let mux_16_8w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        let out = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+    }
+    #[test]
+    fn test_dmux_4w() {
+        let (sel_1, sel_2) = (sel[0], sel[1]);
+    }
+    #[test]
+    fn test_dmux_8w() {
+        let out = [1, 0, 1, 0, 1, 0, 1, 0,];
+    }
+    fn test_not() {
+        let not_1 = not(1);
+        let not_2 = not(0);
+
+        assert_eq!(not_1, 0);
+        assert_eq!(not_2, 1);
+    }
+    #[test]
+    fn test_not_16() {
+
+    }
+
 }

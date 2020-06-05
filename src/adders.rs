5d80566707c0f68496f87378e1cde447f94abab9
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-06-04 22:27:24 -0700

    Alright, modules set up, half adder gate works!

--- src/adders.rs
+++ src/adders.rs
@@ -3,13 +3,19 @@ pub mod adders {
 
     pub fn half_adder(a: i32, b: i32) -> (i32, i32) {
         if a + b == 2 {
-            let sum = 1;
-            let carry = 1;
-            return (sum, carry)
+            return (1, 1)
+        }
+        else if a == 1 && b == 0 {
+            return (1, 0)
+        }
+        else if a == 0 && b  == 1 {
+            return (1, 0)
         }
         else {
             return (0, 0)
         }
 
+
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
-// use crate::l_g::{and, or_nand, or_8w};
 use nand2_tetris::l_g::l_g::{mux_16_4w};
+use nand2_tetris::adders::adders::{half_adder};
+
 fn main() {
-          let mux_16_4w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-        let mux_16_4w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
-        let mux_16_4w_arr_3 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-        let mux_16_4w_arr_4 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
-        let out = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-        println!("Mux164w: {:?}", nand2_tetris::l_g::l_g::mux_16_4w(mux_16_4w_arr_1, 
-                                            mux_16_4w_arr_2, 
-                                            mux_16_4w_arr_3, 
-                                            mux_16_4w_arr_4, 
-                                            [0, 0]));
-    }
-    // println!("{}", or_nand(1, 0));
-    // println!("{:?}", and_16(and_16_arr_1, and_16_arr_2, &mut out));
-    // println!("mux_16 is: {:?}", mux_16(and_16_arr_2, and_16_arr_1, 0));
-    // println!("or_8w is: {:?}", l_g::or_8w(or_8w_arr_1, or_8w_arr_2));
-    // println!("{}", or(0, 0));
-    // println!("mux_16 is: {:?}", mux_16(and_16_arr_1, out, 1));
+  let mux_16_4w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+  let mux_16_4w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+  let mux_16_4w_arr_3 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+  let mux_16_4w_arr_4 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+  let out = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+  println!("Half adder: {:?}", half_adder(0,0));
+}

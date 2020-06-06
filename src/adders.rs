6a8078c7824c61f7981881671a7651f6568deedc
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-06-06 15:19:02 -0700

    Working on adders, docs and tdd

--- src/adders.rs
+++ src/adders.rs
@@ -1,21 +1,102 @@
 
 pub mod adders {
-
+    /// Adds two bits, where the least significant bit of the addition is 
+    /// called sum, and the most significant bit is called carry.
+    ///
+    /// #Examples
+    ///
+    /// ```
+    /// let a = 1;
+    /// let b = 1;
+    /// let answer = half_adder(a, b); 
+    /// assert_eq!(answer, (1, 0));
+    /// 
+    /// let c = 1
+    /// let d = 0
+    /// let answer_2 = half_adder(c, d)
+    /// assert_eq!(answer_2, (0, 1))
+    ///
+    /// ```
+    ///
+    /// Values will be returned in a tuple, as (carry, sum) respectively.
     pub fn half_adder(a: i32, b: i32) -> (i32, i32) {
-        if a + b == 2 {
-            return (1, 1)
+        if a == 1 && b == 1 {
+            return (1, 0)
         }
         else if a == 1 && b == 0 {
-            return (1, 0)
+            return (0, 1)
         }
         else if a == 0 && b  == 1 {
-            return (1, 0)
+            return (0, 1)
         }
         else {
             return (0, 0)
         }
 
 
+    }
+    /// Adds three bits, where the least significant bit of the addition is 
+    /// called sum, and the most significant bit is called carry.
+    ///
+    /// #Examples
+    ///
+    /// ```
+    /// let a = 1;
+    /// let b = 1;
+    /// let c = 1
+    /// let answer = full_adder(a, b, c); 
+    /// assert_eq!(answer, (1, 1));
+    /// let d = 0
+    /// let e = 0
+    /// let f = 1
+    /// 
+    /// let answer_2 = full_adder(d, e, f)
+    /// assert_eq!(answer_2, (0, 1))
+    ///
+    /// ```
+    ///
+    /// Values will be returned in a tuple, as (carry, sum) respectively.
+    pub fn full_adder(a: i32, b: i32, c: i32) -> (i32, i32) {
+        if a == 0 && b == 0 && c == 0  {
+            return (0, 0)
+        }
+        else if a == 0 && b == 0 && c == 1  {
+            return (0, 1)
+        }
+        else if a == 0 && b == 1 && c == 0  {
+            return (0, 1)
+        }
+        else if a == 0 && b == 1 && c == 1  {
+            return (1, 0)
+        }
+        else if a == 1 && b == 0 && c == 0  {
+            return (0, 1)
+        }
+        else if a == 1 && b == 0 && c == 1  {
+            return (1, 0)
+        }
+        else if a == 1 && b == 1 && c == 0  {
+            return (1, 0)
+        }
+        else if a == 1 && b == 1 && c == 1  {
+            return (1, 1)
+        }
+        else {
+            panic!("Invalid input")
+        }
+
+
+
+    }
+
+    pub fn add_16(array_a: [i32; 16], array_b: [i32; 16]) -> [i32;16] {
+        let mut out: [i32; 16] = [1; 16];
+
+        for i in 0..16 {
+            out[i] = half_adder(array_a[i], array_b[i]).1
+        }
+
+        return out
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use nand2_tetris::l_g::l_g::{mux_16_4w};
-use nand2_tetris::adders::adders::{half_adder};
-
+use nand2_tetris::adders::adders::{half_adder, add_16};
+#[allow(unused_variables)]
 fn main() {
   let mux_16_4w_arr_1 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-  let mux_16_4w_arr_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
   let mux_16_4w_arr_3 = [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-  let mux_16_4w_arr_4 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+  
+  
+  
+  let mux_16_4w_arr_2 = [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+  let mux_16_4w_arr_4 = [0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
   let out = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
-  println!("Half adder: {:?}", half_adder(0,0));
+  println!("Add 16 : {:?}", add_16(mux_16_4w_arr_4, mux_16_4w_arr_2));
 }
